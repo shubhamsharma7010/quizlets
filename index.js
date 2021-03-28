4e887702f1162ce25108b0d539d6b588ed3b42d7
ShubhamSharma70 <shubhsharma7010@gmail.com>
2021-03-28 08:28:46 +0000

    updated options

--- index.js
+++ index.js
@@ -1,34 +1,70 @@
-var readlineSync = require("readline-sync");
-var userName = readlineSync.question(" May I know Your Name ? ");
-console.log(" Welcome!! " + userName + " Would You Like To Hop In For a Quiz? ");
+var readlineSync = require('readline-sync');
+var chalk = require("chalk")
+
+var userName = chalk.bold.red(readlineSync.question(chalk.blue(("What's your name?  "))));
+
+console.log("Welocome to the quiz " + userName + "\n");
+console.log("Topic of the quiz is" + chalk.yellowBright("General Knowledge"))
 var score = 0;
-//function 
-function play(question, answer)
-{
-  var userAnswer = readlineSync.question(question);
 
-  if (userAnswer === answer)
-  {
-    console.log(" You are Right!! ");
-    score = score + 1;
-  }
-  else
-  {
-    console.log(" You are wrong!! " );
-  }
-  console.log(" Your current score: ", score);
-  console.log("------------");
-};
-//array
+function quiz(options, question, answer){
+  var index = readlineSync.keyInSelect(options, chalk.yellow(question));
+  var userAnswer = options[index];
+  
+  var answerIndex = options.indexOf(answer)
 
-var questions = [{question: " Q1.what is the highest peak in the world", answer: "mount everest"},{question: " Q2.which is the highest civilian award in our county? ",answer:"bharat ratna"},{
-  question: " Q3.what is the currency of china? ",answer:"renminbi"},{question: " Q4.which is the highest military award in our country? ",answer: "param vir chakra"},{
-    question: " Q5.How many players are there in cricket? ", answer: "11"}
-];
-//loop
-for(var i=0; i<questions.length; i++){
-  currentQuestion = questions[i];
-  play(currentQuestion.question,currentQuestion.answer)
+  if(userAnswer === answer){
+    console.log(chalk.underline.blue("Correct!"))
+    score=score + 1;
+  
+  }else if(index == "-1"){
+    console.log("You choose to skip this question")
+  }
+  else{
+    console.log(chalk.underline.red("Incorrect!"));  
+    console.log("Correct answer is " + (answerIndex + 1) + " " +  chalk.bgYellow(answer))
+
+  }
+  console.log(chalk.bgRed("Your score is : ",score))
+  console.log("\n")
 }
 
-console.log(" Yay!! your final score is: ",score);
+var questions = [{
+  options : ["Mount Everest","Kanchenjunga","Alps","Pyrenees"],
+  question : "Which is the Highest peak in the world ?",
+  answer : "Mount Everest"
+},{
+  options : ["Bharat Ratna","Padmabhushan","Paramvir Chakra","Padma shri"],
+  question : "Which is the highest Civilian Award in India?",
+  answer : "Bharat Ratna"
+},{
+  options : ["Mahatma Gandhi","Jawahar Lal Nehru","Indira Gandhi","Sardar Patel"],
+  question : "Who was the first Prime Minister of Independent India?",
+  answer : "Jawahar Lal Nehru"
+},{
+  options : ["15-8-1949","26-1-1950","26-1-1951","15-8-1950"],
+  question : "When did the constitution of India come into effect ?",
+  answer : "26-1-1950"
+},{
+  options : ["China","Singapore","India","Japan"],
+  question : "Where were the first Asian Games held??",
+  answer : "India"
+}]
+
+
+
+for(var i=0; i<questions.length; i++){
+  currentQuestion = questions[i];
+  quiz(currentQuestion.options,currentQuestion.question, currentQuestion.answer);
+}
+
+console.log("You scored " + score + "/" + 5 + ".")
+if(score == 0){
+  console.log(chalk.bold.redBright("Better luck next time!"))
+}else if(score == 1 || score == 2){
+  console.log(chalk.bold.redBright("You can do better!"))
+}else if(score == 3 || score == 4){
+  console.log(chalk.bold.redBright("It was close! You almost did it!"))
+}else{
+  console.log(chalk.bold.redBright("You nailed it!"))
+}
